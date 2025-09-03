0d27eb7ee4082d1adc26cb4d67d2d567111cf2e2
Abdur Rahaman <phd2401141001@iiti.ac.in>
2025-09-03 23:10:30 +0530

    Update text_analyzer.cpp

--- text_analyzer.cpp
+++ text_analyzer.cpp
@@ -5,3 +5,23 @@ std::pair<int,int> analyzeText(const std::string& text) {
     // TODO: Count vowels and consonants
     return {0,0}; // Placeholder
 }
+
+
+
+// #include <iostream>
+// #include <string>
+// #include <cctype>
+
+// std::pair<int,int> analyzeText(const std::string& text) {
+//     int vowels = 0, consonants = 0;
+//     for (char ch : text) {
+//         if (std::isalpha(ch)) {
+//             char c = std::tolower(ch);
+//             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+//                 vowels++;
+//             else
+//                 consonants++;
+//         }
+//     }
+//     return {vowels, consonants};
+// }

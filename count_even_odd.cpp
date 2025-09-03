e77a4e65c6374fb812889dd9199a8fd73e7134be
Abdur Rahaman <phd2401141001@iiti.ac.in>
2025-09-03 23:09:51 +0530

    Update count_even_odd.cpp

--- count_even_odd.cpp
+++ count_even_odd.cpp
@@ -5,3 +5,13 @@ std::pair<int,int> countEvenOdd(const std::vector<int>& nums) {
     // TODO: Count evens and odds
     return {0,0}; // Placeholder
 }
+
+
+// std::pair<int,int> countEvenOdd(const std::vector<int>& nums) {
+//     int even = 0, odd = 0;
+//     for (int num : nums) {
+//         if (num % 2 == 0) even++;
+//         else odd++;
+//     }
+//     return {even, odd};
+// }
